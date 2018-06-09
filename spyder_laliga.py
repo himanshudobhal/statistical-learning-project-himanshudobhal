cdadb30ecd35b1335b01672116e065b0d1931e2f
dobhalhimanshu <himanshu@savormetrics.com>
2018-06-09 19:26:07 +0530

    stat_assignment

---
+++ spyder_laliga.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 17:14:55 2018
+
+@author: Himanshu
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.array([1.9921,1.9624,1.9101,1.8062,1.5611,1.5132,1.2330,1.0531])
+x = np.array([1,2,5,10,20,30,40,50])
+plt.plot(x,y)
+par = np.polyfit(x, y, 1, full=True)
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image, 
+    as illustrated in Figure 1.
+    
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+    
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line)
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=0)
+    ### END CODE HERE ###
+    
+    return X_pad
+    
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print ("x.shape =", x.shape)
+print ("x_pad.shape =", x_pad.shape)
+print ("x[1, 1] =", x[1, 1])
+print ("x_pad[1, 1] =", x_pad[1, 1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
